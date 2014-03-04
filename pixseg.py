cda98c737017b82d74006da33b93f0f74871a063
Eric Martin <e.a.martin1337@gmail.com>
2014-03-04 02:45:21 -0800

    now resizing images for faster pixseg

--- pixseg.py
+++ pixseg.py
@@ -6,6 +6,7 @@ import subprocess
 import sys
 
 import numpy as np
+from scipy.misc import imread, imresize, imsave
 
 PIXSEG_DIR = 'pixseg/'
 EXECUTABLE = os.path.join(PIXSEG_DIR, 'inferPixelLabels')
@@ -18,28 +19,32 @@ OUTPUT_SUFFIX = '.out.txt'
 
 def get_pix_labels(image_file, delete=True):
     file_name = os.path.basename(image_file)
-
     temp_image_path = os.path.join(PIXSEG_DIR, file_name)
-    shutil.copy(image_file, temp_image_path)
+
+    # resize image
+    image = imread(os.path.expanduser(image_file))
+    original_size = image.shape[:-1]
+    smaller = imresize(image, 300.0 / max(image.shape))
+    imsave(temp_image_path, smaller)
+
+    #shutil.copy(image_file, temp_image_path)
 
     with open(INPUT_FILE, 'w') as f:
         f.write(file_name + '\n')
 
     command = ('%s %s -config %s -outLabels %s %s' %
                (SETUP, EXECUTABLE, CONFIG, OUTPUT_SUFFIX, INPUT_FILE))
-    print command
 
     with open('/dev/null') as devnull:
         subprocess.check_call(command, shell=True,
                               stdout=devnull, stderr=devnull)
 
     with open(temp_image_path + OUTPUT_SUFFIX) as f:
-        reader = csv.reader(f, delimiter=' ')
-        lines = list(reader)
+        lines = f.readlines()
 
-    lines = [map(int, l) for l in lines]
-    data = np.array(lines)
-    print data
+    lines = [map(int, l.split()) for l in lines]
+    data = imresize(np.array(lines, dtype=np.uint8),
+                    original_size, interp='nearest')
 
     os.remove(temp_image_path)
     os.remove(INPUT_FILE)
